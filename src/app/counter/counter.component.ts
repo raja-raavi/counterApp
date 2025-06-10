6e8b3783a41853db42f81e09b65a62e6e6705ac3
raja-raavi <rajaraavi999@gmail.com>
2025-06-10 23:42:03 +0530

    Implemented history component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ViewChild } from '@angular/core';
+import { TrackHistoryComponent } from '../track-history/track-history.component';
 
 @Component({
   selector: 'app-counter',
@@ -7,16 +8,25 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
   count = 0;
+  history!: number[];
+  @ViewChild('trackHistory') tracking!: TrackHistoryComponent;
+
+  ngAfterViewInit(): void {
+    this.tracking.addEntry('Reset', this.count); // Initial entry
+  }
 
   increment() {
     this.count++;
+    this.tracking.addEntry('Increment', this.count);
   }
 
   decrement() {
     this.count--;
+    this.tracking.addEntry('Decrement', this.count);
   }
 
   reset() {
     this.count = 0;
+    this.tracking.addEntry('Reset', this.count);
   }
 }
--- src/app/track-history/track-history.component.ts
+++ src/app/track-history/track-history.component.ts
@@ -5,4 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './track-history.component.html',
   styleUrls: ['./track-history.component.css'],
 })
-export class TrackHistoryComponent {}
+export class TrackHistoryComponent {
+  trackHistory: string[] = [];
+
+  addEntry(action: 'Increment' | 'Decrement' | 'Reset', value: number) {
+    let entryText = '';
+
+    switch (action) {
+      case 'Increment':
+        entryText = `[+1] Counter is now ${value}`;
+        break;
+      case 'Decrement':
+        entryText = `[-1] Counter is now ${value}`;
+        break;
+      case 'Reset':
+        entryText = `[Reset] Counter is now ${value}`;
+        break;
+    }
+
+    this.trackHistory.push(entryText);
+  }
+
+  clearHistory() {
+    this.trackHistory.length = 0;
+  }
+}
