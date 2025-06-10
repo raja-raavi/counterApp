7f465ab7e39d7c34b34de0e59604e6676d86d9be
raja-raavi <rajaraavi999@gmail.com>
2025-06-10 21:35:04 +0530

    components created

---
+++ src/app/track-history/track-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-history',
+  templateUrl: './track-history.component.html',
+  styleUrls: ['./track-history.component.css']
+})
+export class TrackHistoryComponent {
+
+}
